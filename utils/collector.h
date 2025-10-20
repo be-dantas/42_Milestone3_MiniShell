1394b6f863852912433af2915433863fb6e613c5
wees-s <wesleyalexandre3946@gmail.com>
2025-10-20 17:19:08 -0300

    update -> name collection and main_teste

--- utils/collector.h
+++ utils/collector.h
@@ -6,7 +6,7 @@
 /*   By: bedantas <bedantas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 11:00:29 by wedos-sa          #+#    #+#             */
-/*   Updated: 2025/10/20 13:36:00 by bedantas         ###   ########.fr       */
+/*   Updated: 2025/10/20 17:05:00 by wedos-sa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,16 @@
 
 #include "../libft/libft.h"
 
-typedef struct s_collect
+typedef struct	s_collect
 {
 	struct s_collect	*next;
 	void				*data;
 }	t_collect;
+
+t_collect   *create_item(void *data);
+t_collect	*last_item(t_collect *begin_list);
+t_collect   *init_collect(void);
+void		close_collect(t_collect *init);
+void		*put_item(t_collect **begin_list, void *data);
 
 #endif
