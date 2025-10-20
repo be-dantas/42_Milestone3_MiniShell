1be2c60853eb054915a21e1e8019cf93f090b373
wees-s <wesleyalexandre3946@gmail.com>
2025-10-20 17:30:15 -0300

    update -> delete garbage.h and merge in minishell.h

--- utils/minishell.h
+++ utils/minishell.h
@@ -6,14 +6,25 @@
 /*   By: bedantas <bedantas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 10:58:36 by wedos-sa          #+#    #+#             */
-/*   Updated: 2025/10/20 17:17:14 by wedos-sa         ###   ########.fr       */
+/*   Updated: 2025/10/20 17:27:46 by wedos-sa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include "garbage_collection.h"
 # include "../libft/libft.h"
+
+typedef struct  s_collect
+{
+    struct s_collect    *next;
+    void                *data;
+}   t_collect;
+
+t_collect	*create_item(void *data);
+t_collect	*last_item(t_collect *begin_list);
+t_collect	*init_collect(void);
+void		close_collect(t_collect *init);
+void		*put_item(t_collect **begin_list, void *data);
 
 #endif
