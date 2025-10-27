186e62c357466b7505e38b28065199fe3b3c5915
wees-s <wesleyalexandre3946@gmail.com>
2025-10-27 15:34:44 -0300

    add -> check_list, faz checagem dos itens na lista para acrescentar ou modificar um valor usando a funcao export

---
+++ src/check_list.c
@@ -0,0 +1,29 @@
+#include "../utils/minishell.h"
+
+void	check_to_put(char *string, t_env **begin_list)
+{
+	char	**temp;
+	t_env	*list_reset;
+	t_env	*ptr;
+
+	ptr = NULL;
+	list_reset = *begin_list;
+	temp = split_env(string);
+	while (*begin_list)
+	{
+		if (ft_strncmp(ptr->key, temp[0], ft_strlen(ptr->key)) == 0)
+		{
+			ptr->value = temp[1];
+			free(temp[0]);
+			free(temp[1]);
+			free(temp);
+			return ;
+		}
+		(*begin_list) = (*begin_list)->next;
+	}
+	(*begin_list) = list_reset;
+	put_env(begin_list, string);
+	free(temp[0]);
+	free(temp[1]);
+	free(temp);
+}
