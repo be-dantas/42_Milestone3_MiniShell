9c041b90dfaadefe8f3c2d614e1435da30960789
wees-s <wesleyalexandre3946@gmail.com>
2025-10-29 16:59:17 -0300

    updates

--- src/export/check_list.c
+++ src/export/check_list.c
@@ -5,14 +5,27 @@ void	check_to_put(char *split_line, t_env **new_env)
 	char	**temp;
 	t_env	*list_reset;
 	t_env	*ptr;
+	char	*temp_arg;
 
+	temp_arg = NULL;
 	ptr = NULL;
 	list_reset = *new_env;
 	temp = split_env(split_line);
 	while (*new_env)
 	{
-		if (ft_strncmp((*new_env)->key, temp[0], ft_strlen((*new_env)->key)) == 0)
+		if (ft_strncmp((*new_env)->key, temp[0], ft_strlen((*new_env)->key) + 1) == 0)
 		{
+			// abaixo disso
+			temp_arg = expand_arg(*new_env, temp[1]);
+			if (temp_arg != NULL)
+			{
+				update_value(temp_arg, (*new_env));
+				free(temp[0]);
+				free(temp[1]);
+				free(temp);
+				return ;
+			}
+			// acima disso
 			update_value(temp[1], (*new_env));
 			free(temp[0]);
 			free(temp[1]);
