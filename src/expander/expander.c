766e5663a675b001171b8ef6cf0db55429a95ce2
Beatriz Dantas <226878089+be-dantas@users.noreply.github.com>
2025-10-31 15:06:40 -0300

    refatorar

--- src/expander/expander.c
+++ src/expander/expander.c
@@ -1,51 +1,4 @@
 #include "../../utils/minishell.h"
-/*
-char	*expand_arg(t_env *begin_list, char *string)
-{
-	int		i;
-	int		j;
-	char	*result;
-	char	*temp;
-
-	temp = NULL;
-	result = NULL;
-	j = 1;
-	i = 0;
-	while (string[i] != '$' && string[i] != '\0')
-		i++;
-	if (string[i] != '$')
-		return (NULL);
-	if (i == 0)
-	{
-		while (string[j] != '\0')
-			j++;
-		temp = malloc(sizeof(char) * 1); //malloc
-		temp[0] = '\0';
-		result = ft_strjoin(temp, expanded(begin_list, string + 1)); //malloc
-	}
-	if (i > 0)
-	{
-		temp = malloc(sizeof(char) * i + 1); //malloc
-		ft_strlcpy(temp, string, i + 1);
-		result = ft_strjoin(temp, expanded(begin_list, string + i + 1)); //malloc
-	}
-	if (temp != NULL)
-		free(temp);
-	return (result);
-}
-
-char	*expanded(t_env *begin_list, char *key)
-{
-	while (begin_list)
-	{
-		if (ft_strncmp(key, begin_list->key, ft_strlen(key) + 1) == 0)
-			return (begin_list->value);
-		begin_list = begin_list->next;
-	}
-	return (NULL);
-}*/
-
-#include "../../utils/minishell.h"
 
 static char	*get_key(char *str)
 {
@@ -62,58 +15,57 @@ static char	*get_key(char *str)
 	return (key);
 }
 
-static void	join_and_free(char **result, char *add)
+static char	*join_and_free(char *s1, const char *s2)
 {
-	char	*tmp;
+    char *res;
 
-	tmp = *result;
-	*result = ft_strjoin(*result, add);
-	free(tmp);
+    if (!s1 && !s2)
+        return (NULL);
+    if (!s1)
+        return (ft_strdup(s2));
+    if (!s2)
+        return (s1);
+    res = ft_strjoin(s1, s2);
+    free(s1);
+    return (res);
 }
 
 char	*expand_arg(t_env *begin_list, char *str)
 {
-	int		i;
-	int		in_single;
-	int		in_double;
-	char	*result;
+    int		i;
+    int		in_single;
+    int		in_double;
+    char	*result;
 
-	i = 0;
-	in_single = 0;
-	in_double = 0;
-	result = ft_strdup("");
-	while (str[i])
-	{
-		printf("VEZES QUE EU ENTREI NO LAÇO = %d\n", i);
-		if (str[i] == '\'' && !in_double)
-		{
-			in_single = !in_single;	
-			printf("VALOR DO IN_SINGLE = %d\n", in_double);
-		}
-		else if (str[i] == '"' && !in_single)
-		{
-			in_double = !in_double;
-			printf("VALOR DO IN_DOUBLE = %d\n", in_double);
-		}
-		else if (str[i] == '$' && !in_single && str[i + 1])
-		{
-			char *key = get_key(&str[i + 1]);
-			char *value = expanded(begin_list, key);
-			if (!value)
-				value = "";
-			join_and_free(&result, value);
-			i += ft_strlen(key);
-			free(key);
-		}
-		else
-		{
-			char *literal = ft_substr(str, i, 1);
-			join_and_free(&result, literal);
-			free(literal);
-		}
-		i++;
-	}
-	return (result);
+    i = 0;
+    in_single = 0;
+    in_double = 0;
+    result = ft_strdup("");
+    while (str[i])
+    {
+        if (str[i] == '\'' && !in_double)
+            in_single = !in_single;	
+        else if (str[i] == '"' && !in_single)
+            in_double = !in_double;
+        else if (str[i] == '$' && !in_single && str[i + 1])
+        {
+            char *key = get_key(&str[i + 1]);
+            char *value = expanded(begin_list, key);
+            if (!value)
+                value = "";
+            result = join_and_free(result, value);
+            i += ft_strlen(key);
+            free(key);
+        }
+        else
+        {
+            char *literal = ft_substr(str, i, 1);
+            result = join_and_free(result, literal);
+            free(literal);
+        }
+        i++;
+    }
+    return (result);
 }
 
 char	*expanded(t_env *begin_list, char *key)
--- src/export/check_list.c
+++ src/export/check_list.c
@@ -2,52 +2,57 @@
 
 void	check_to_put(char *split_line, t_env **new_env)
 {
+	int		i;
 	int		flag;
-	char	**temp;
 	t_env	*list_reset;
 	char	*temp_expand;
-	int		i;
+	char	**temp;
 
 	i = 0;
-	temp_expand = expand_arg(*new_env, split_line);
 	flag = 0;
 	list_reset = *new_env;
+	temp_expand = expand_arg(*new_env, split_line);
 	temp = split_env(split_line);
-	//printf("%s", temp_expand);
-	while (*new_env)
-	{
-		if (ft_strncmp((*new_env)->key, temp[0], ft_strlen((*new_env)->key) + 1) == 0)
-		{
-			//abaixo disso
-			if (temp_expand != NULL)
-			{
-				while (*temp_expand != '=')
-					temp_expand++;
-				temp_expand++;
-				temp[1] = temp_expand;
-			}
-			//acima disso
-			//printf("%s\n\n\n", temp[1]);
-			update_value(temp[1], (*new_env));
-			flag = 1;
-			break;
-		}
-		(*new_env) = (*new_env)->next;
-	}
+    while (*new_env)
+    {
+        if (ft_strncmp((*new_env)->key, temp[0], ft_strlen((*new_env)->key) + 1) == 0)
+        {
+            if (temp_expand != NULL)
+            {
+                char *eq = ft_strchr(temp_expand, '=');
+                char *val_dup;
+
+                if (eq && *(eq + 1))
+                    val_dup = ft_strdup(eq + 1);
+                else
+                    val_dup = ft_strdup("");
+                free(temp[1]);
+                temp[1] = val_dup;
+            }
+            update_value(temp[1], (*new_env));
+            flag = 1;
+            break;
+        }
+        (*new_env) = (*new_env)->next;
+    }
 	if (!flag)
 	{
-		//abaixo disso
 		if (temp_expand != NULL)
 		{
 			(*new_env) = list_reset;
 			put_env(new_env, temp_expand);
+            free(temp[0]);
+            free(temp[1]);
+            free(temp);
+			free(temp_expand);
 			return ;
 		}
-		//acima disso
 		(*new_env) = list_reset;
 		put_env(new_env, split_line);
 	}
 	(*new_env) = list_reset;
+	if (temp_expand != NULL)
+        free(temp_expand);
 	free(temp[0]);
 	free(temp[1]);
 	free(temp);
