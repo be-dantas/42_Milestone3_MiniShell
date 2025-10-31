8314f21fc2a8c71fb9f8ed180cab1a0967d864cc
Beatriz Dantas <226878089+be-dantas@users.noreply.github.com>
2025-10-31 15:35:42 -0300

    criando branch

--- utils/minishell.h
+++ utils/minishell.h
@@ -62,17 +62,4 @@ int			main(int argc, char **argv, char **envp);
 //unset.c
 void		unset_env(t_env **begin_list, char *string);
 
-// collection.c
-typedef struct s_collect
-{
-	struct s_collect	*next;
-	void				*data;
-}	t_collect;
-
-t_collect	*create_item(void *data);
-t_collect	*last_item(t_collect *begin_list);
-t_collect	*init_collect(void);
-void		close_collect(t_collect *init);
-void		*put_item(t_collect **begin_list, void *data);
-
 #endif
