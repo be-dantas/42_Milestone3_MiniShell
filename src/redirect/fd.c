9acef810f6c8dac91349ffd920bd1c14a1a3b81a
wees-s <wesleyalexandre3946@gmail.com>
2025-11-07 14:38:40 -0300

    update fd

--- src/redirect/fd.c
+++ src/redirect/fd.c
@@ -1,5 +1,97 @@
 #include "../../utils/minishell.h"
 
+#include "../../utils/minishell.h"
+
+static void	func_flag(char *line, int *i, int quote[2])
+{
+	while (line[*i] && line[*i] != '<' && line[*i] != '>')
+	{
+		if (line[*i] == '\'')
+			quote[0] = !quote[0];
+		else if (line[*i] == '\"')
+			quote[1] = !quote[1];
+		(*i)++;
+	}
+}
+
+static int	open_write_fd(char *line, int i)
+{
+	int		start;
+	char	*file;
+	int		fd;
+
+	// pula '>' e possíveis espaços
+	if (line[i + 1] == '>')
+		i += 2;
+	else
+		i++;
+	while (line[i] == ' ')
+		i++;
+
+	start = i;
+	while (line[i] && !ft_isspace(line[i]) && line[i] != '<' && line[i] != '>')
+		i++;
+
+	file = ft_substr(line, start, i - start);
+	if (line[start - 2] == '>')
+		fd = open(file, O_WRONLY | O_CREAT | O_APPEND, 0644);
+	else
+		fd = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	free(file);
+	return (fd);
+}
+
+static int	open_read_fd(char *line, int i)
+{
+	int		start;
+	char	*file;
+	int		fd;
+
+	i++; // pula '<'
+	while (line[i] == ' ')
+		i++;
+	start = i;
+	while (line[i] && !ft_isspace(line[i]) && line[i] != '<' && line[i] != '>')
+		i++;
+
+	file = ft_substr(line, start, i - start);
+	fd = open(file, O_RDONLY, 0444);
+	free(file);
+	return (fd);
+}
+
+int	*parse_fd(char *line)
+{
+	int		i;
+	int		quote[2];
+	int		*fd;
+
+	fd = malloc(sizeof(int) * 2);
+	if (!fd)
+		return (NULL);
+	fd[0] = dup(STDIN_FILENO);  // entrada padrão
+	fd[1] = dup(STDOUT_FILENO); // saída padrão
+	quote[0] = 0;
+	quote[1] = 0;
+	i = 0;
+	while (line[i])
+	{
+		func_flag(line, &i, quote);
+		if (!quote[0] && !quote[1])
+		{
+			if (line[i] == '<')
+				fd[0] = open_read_fd(line, i);
+			else if (line[i] == '>')
+				fd[1] = open_write_fd(line, i);
+		}
+		if (line[i])
+			i++;
+	}
+	return (fd);
+}
+
+
+/*
 void	func_flag(char *line, int *end, int flag[2])
 {
 	while (line[*end] && line[*end] != '<' && line[*end] != '>')
@@ -94,4 +186,4 @@ int	*fd(char *line)
 		i++;
 	}
 	return (fd);
-}
+}*/
