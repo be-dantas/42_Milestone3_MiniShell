ecd6db4d38a5325d2007a6b1a7a27b2296737652
Beatriz Dantas <226878089+be-dantas@users.noreply.github.com>
2025-11-07 14:05:17 -0300

    subindo

--- src/redirect/redirect.c
+++ src/redirect/redirect.c
@@ -1,119 +1,39 @@
 #include "../../utils/minishell.h"
 
-static void	exec_line(char **line_tokens, t_env *new_env)
+// static void	exec_line(char **line_tokens, t_env *new_env)
+// {
+// 	if (ft_strncmp(line_tokens[0], "echo", 5) == 0)
+// 		echo(line_tokens, new_env);
+// 	else if (ft_strncmp(line_tokens[0], "cd", 3) == 0)
+// 		;
+// 	else if (ft_strncmp(line_tokens[0], "pwd", 4) == 0)
+// 		;
+// 	else if (ft_strncmp(line_tokens[0], "export", 7) == 0)
+// 		export_arg(line_tokens, new_env);
+// 	else if (ft_strncmp(line_tokens[0], "unset", 6) == 0)
+// 		unset_env(&new_env, line_tokens);
+// 	else if (ft_strncmp(line_tokens[0], "env", 4) == 0)
+// 		print_env(new_env);
+// 	else if (ft_strncmp(line_tokens[0], "exit", 5) == 0)
+// 		read_input("exit", new_env);
+// 	else
+// 		printf("Command not found\n"); //VOCÊ VAI REMOVER NO FUTURO E SUBSTUIR PELA FUNÇÃO QUE FAZ O EXECVE
+// }
+
+int	*func_flag(char *line, int *end)
 {
-	if (ft_strncmp(line_tokens[0], "echo", 5) == 0)
-		echo(line_tokens, new_env);
-	else if (ft_strncmp(line_tokens[0], "cd", 3) == 0)
-		;
-	else if (ft_strncmp(line_tokens[0], "pwd", 4) == 0)
-		;
-	else if (ft_strncmp(line_tokens[0], "export", 7) == 0)
-		export_arg(line_tokens, new_env);
-	else if (ft_strncmp(line_tokens[0], "unset", 6) == 0)
-		unset_env(&new_env, line_tokens);
-	else if (ft_strncmp(line_tokens[0], "env", 4) == 0)
-		print_env(new_env);
-	else if (ft_strncmp(line_tokens[0], "exit", 5) == 0)
-		read_input("exit", new_env);
-	else
-		printf("Command not found\n"); //VOCÊ VAI REMOVER NO FUTURO E SUBSTUIR PELA FUNÇÃO QUE FAZ O EXECVE
-}
+	int	*flag[2];
 
-char *remove_word(const char *str, const char *word)
-{
-	char *result;
-	char *pos;
-	size_t len_word = ft_strlen(word);
-	size_t len_before, len_after;
-	const char *after;
-
-	pos = strstr(str, word);
-	if (!pos)
-		return ft_strdup(str); // palavra não encontrada
-
-	len_before = pos - str;
-	after = pos + len_word;
-	while (*after == ' ') // pula espaços depois da palavra
-		after++;
-	len_after = ft_strlen(after);
-
-	// +2 para espaço e '\0'
-	result = calloc(len_before + len_after + 2, 1);
-	if (!result)
-		return NULL;
-
-	// copia o que vem antes
-	ft_strlcat(result, str, len_before + 1);
-
-	// se precisar, adiciona espaço entre as partes
-	if (len_before > 0 && *after != '\0' && result[len_before - 1] != ' ')
-		ft_strlcat(result, " ", len_before + len_after + 2);
-
-	// adiciona o que vem depois da palavra
-	ft_strlcat(result, after, len_before + len_after + 2);
-
-	return result;
-}
-
-int	*fd(char *line) //retornar fd da linha, leitura e escrita
-{
-	int		i;
-	int		flag[2];
-	int		fd[2];
-
-	i = 0;
-	while (line[i] != '\0')
+	while (line[*end] && line[*end] != '<' && line[*end] != '>')
 	{
-		flag = flag(line, &i, flag);
-		if (line[i] == '<' && flag[0] == 0 && flag[1] == 0)
-		{
-			if (line[i + 1] == '<')
-				//função para << ;
-			else
-				fd[0] = open(NOME_ARQUIVO, O_RDONLY, 0444);;
-		}
-		else if (line[i] == '>' && flag[0] == 0 && flag[1] == 0)
-		{
-			if (line[i + 1] == '>')
-				fd[1] = open(NOME_ARQUIVO, O_WRONLY | O_CREAT | O_APPEND, 0644);
-			else
-				fd[1] = open(NOME_ARQUIVO, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-		}
-		i++;
-
-
-			//split1 = ft_split(line, '>');
-			//palavra1 = split1[0];
-			//fd_name_temp = split1[1]; //palavra1 ""arquivo.txt palavra2""
-			//fd_name = ft_split(fd_name_temp, ' ')[0];
-			//palavra2 = remove_word(fd_name_temp, fd_name);
-			//result = ft_strjoin(palavra1, palavra2);
-			//final = ft_strjoin(final, result);
-			//fd = open(fd_name, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-			//dup2(fd, STDOUT_FILENO);
-			//close(fd);
-			//free(fd_name_temp);
-			//free(fd_name);
-
-		//fd = open(fd_name, O_WRONLY | O_CREAT | O_APPEND, 0644);
-	}
-	return (fd);
-}
-
-
-int	*flag(char *line, int end, int *flag)
-{
-	while (line[end] && line[end] != '<' && line[end] != '>')
-	{
-		if (line[end] == '\'')
+		if (line[*end] == '\'')
 		{
 			if (flag[0] == 1)
 				flag[0] = 0;
 			else
 				flag[0] = 1;
 		}
-		else if (line[i] == '\"')
+		else if (line[*end] == '\"')
 		{
 			if (flag[1] == 1)
 				flag[1] = 0;
@@ -122,9 +42,82 @@ int	*flag(char *line, int end, int *flag)
 		}
 		(*end)++;
 	}
-	if (line[end] == '<' || line[end] == '>')
+	if (line[*end] == '<' || line[*end] == '>')
 		(*end)++;
 	return (flag);
+}
+
+int	fd_write(char *line, int i)
+{
+	int		count;
+	int		restart;
+	int		fd_1;
+	char	*file;
+
+	count = 0;
+	restart = i;
+	while(line[i] == ' ' && line[i])
+		i++;
+	while(ft_isalnum(line[i]) && line[i])
+	{
+		count++;
+		i++;
+	}
+	file = ft_substr(line, i - count, count + 1);
+	if (line[restart + 1] == '>')
+		fd_1 = open(file, O_WRONLY | O_CREAT | O_APPEND, 0644);
+	else 
+		fd_1 = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	free(file);
+	return (fd_1);
+}
+
+int	fd_read(char *line, int i)
+{
+	int		count;
+	int		fd_0;
+	char	*file;
+
+	count = 0;
+	while (line[i] == ' ' && i >= 0)
+		i--;
+	while (ft_isalnum(line[i]) && i >= 0)
+	{
+		count++;
+		i--;
+	}
+	i++;
+	file = ft_substr(line, i, count + 1);
+	fd_0 = open(file, O_RDONLY, 0444);
+	free(file);
+	return (fd_0);
+}
+
+
+int	*fd(char *line)
+{
+	int	i;
+	int	flag[2];
+	int	fd[2];
+
+	i = 0;
+	flag[0] = dup(STDIN_FILENO);
+	flag[1] = dup(STDOUT_FILENO);
+	while (line[i] != '\0')
+	{
+		flag = func_flag(line, &i);
+		if (line[i] == '<' && flag[0] == 0 && flag[1] == 0)
+		{
+			if (line[i + 1] == '<')
+				//função para << ;
+			else
+				fd[0] = fd_read(line, i);
+		}
+		else if (line[i] == '>' && flag[0] == 0 && flag[1] == 0)
+			fd[1] = fd_write(line, &i);
+		i++;
+	}
+	return (fd);
 }
 
 //echo teste > file.txt >> file1.txt teste2
@@ -138,11 +131,11 @@ char	*command(char *line)
 
 	start = 0;
 	end = 0;
-	Command = ft_strdup("");
+	command = ft_strdup("");
 	while (line[end])
 	{
 		while (line[end] && (flag[0] == 1 || flag[1] == 1))
-			flag = flag(line, &end, flag);
+			flag = func_flag(line, &end);
 		if (!command)
 		{
 			palavra = ft_substr(line, start, (end - start));
@@ -157,16 +150,15 @@ char	*command(char *line)
 			while (line[end] == ' ')
 				end++;
 			if (line[end] == '<' && line[end] == '>')
-				break ;
-			palavra = ft_substr(line, start, end - start);
-			command = ft_strjoin(command, palavra);
+				;
+			else
+				palavra = ft_substr(line, start, end - start);
+				command = ft_strjoin(command, palavra);
 		}
 		if (palavra)
 			free(palavra);
 		end++;
 		start = end;
 	}
-	//flag[0] == 0;
-	//flag[0] == 0;
-	return (Command);
+	return (command);
 }
